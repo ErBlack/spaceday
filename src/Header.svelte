fd9f61d7b1643f45bac4e54927b7a2648f8cbde9
ertema <ertema@yandex-team.ru>
2020-09-18 22:58:16 +0300

    rocket

--- src/Header.svelte
+++ src/Header.svelte
@@ -5,10 +5,24 @@ h1 {
     text-align: center;
 }
 
+.rocket {
+    background-image: url("/spaceday/rocket.png");
+    background-size: contain;
+    background-position: center;
+    display: inline-block;
+    background-repeat: no-repeat;
+    width: 1em;
+    height: 1.5em;
+    line-height: 1.4em;
+    white-space: pre;
+    user-select: none;
+    cursor: pointer;
+}
+
 @media all and (max-width: 550px) {
   h1 {
     font-size: 10vw;
   }
 }
 </style>
-<h1>Space Day</h1>
+<h1><span class="rocket">⠀</span> Space Day</h1>
