dbbc00656050afbe6e45f0cbede77fe956d29b2c
ertema <ertema@yandex-team.ru>
2020-09-23 21:40:16 +0300

    reinit sound

--- src/ost.js
+++ src/ost.js
@@ -14,13 +14,18 @@ export const friction = loadSound('/spaceday/friction.mp3');
 export const explosion = loadSound('/spaceday/explosion.mp3');
 
 
+let first = true;
 const initSound = () => {
+    if (first) {
+        first = false;
+        return;
+    }
     [final, main, friction, explosion].forEach(audio => {
         audio.play();
         audio.pause();
     });
 
-    document.removeEventListener(initSound);
+    document.removeEventListener('touchstart', initSound);
 }
 
 document.addEventListener('touchstart', initSound)
